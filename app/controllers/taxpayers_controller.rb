703f633910b7dfee47d40645335a5f06e597b595
JhovanDaniel <jd069@hotmail.com>
2021-11-30 17:13:30 +0000

    Added taxpayer listing page basics

--- app/controllers/taxpayers_controller.rb
+++ app/controllers/taxpayers_controller.rb
@@ -2,4 +2,8 @@ class TaxpayersController < ApplicationController
     def show
         @taxpayer = Taxpayer.find(params[:id])
     end
+    
+    def index
+        @taxpayers = Taxpayer.all
+    end
 end
