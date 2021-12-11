30cdc630d582f27d8d610002eb62efa1cddfb74b
JhovanDaniel <jd069@hotmail.com>
2021-12-11 03:49:24 +0000

    Fixed application helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
     
     helper_method :current_user
+    helper_method :logged_in?
+    
     
     def current_user
         if session[:user_id]
